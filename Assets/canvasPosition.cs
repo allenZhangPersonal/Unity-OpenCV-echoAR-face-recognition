630c96cb0982ff5b04f470dc21da78ae230aabd2
allen <allen2005ca@hotmail.com>
2021-06-12 15:13:59 -0400

    initial commit

---
+++ Assets/canvasPosition.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class canvasPosition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //GameObject.find("Canvas");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
